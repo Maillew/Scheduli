e18dfc701cb278093e3077aa26d501f0ab199963
William Lin <63217833+Maillew@users.noreply.github.com>
2023-10-10 01:38:03 -0400

    Update pdf_to_string.py
    compress the pdf to only contain information surrounding relevant dates using NER

--- pdf_to_string.py
+++ pdf_to_string.py
@@ -1,4 +1,5 @@
 import PyPDF2
+import spacy
 
 def pdf_to_string(pdf_path):
     text = ""
@@ -9,3 +10,30 @@ def pdf_to_string(pdf_path):
             page = pdf_reader.pages[page_number]
             text += page.extract_text()
     return text
+
+def compress_outline(outline):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(outline)
+
+    ranges = []
+    delta = 100 #how far to go away from the inital date
+    for ent in doc.ents:
+        if ent.label_ == 'DATE':
+            print(ent.text, ent.start_char, ent.end_char, ent.label_)
+            lft = max(0,ent.start_char-delta)
+            rit = min(ent.end_char+delta, len(outline)-1)
+            while outline[lft] != '.' and lft > 0:
+                lft-=1
+            while outline[rit] != '.' and rit < len(outline):
+                rit+=1
+            ranges.append((lft,rit))
+
+    set = {0}
+    for i in ranges:
+        for j in range (i[0],i[1]):
+            set.add(j)
+
+    s =""
+    for i in set:
+        s+=outline[i]
+    return s
